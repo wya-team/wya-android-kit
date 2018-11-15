eddb41c3bba9799329a885518a28220f27444f0c
MaoShine <550612711@qq.com>
2018-11-15 15:55:24 +0800

    wya-android-kit项目提交

---
+++ exapmle/src/main/java/com/wya/example/module/uikit/UiKitExampleActivity.java
@@ -0,0 +1,15 @@
+package com.wya.example.module.uikit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wya.example.R;
+
+public class UiKitExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui_kit_example);
+    }
+}
