1ef487e103e6fcce6851358fec3fd79e42f7ff1f
MaoShine <550612711@qq.com>
2018-11-16 10:22:17 +0800

    修改项目依赖库结构

---
+++ exapmle/src/main/java/com/wya/example/module/helper/HelperExampleActivity.java
@@ -0,0 +1,15 @@
+package com.wya.example.module.helper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wya.example.R;
+
+public class HelperExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_helper);
+    }
+}
---
+++ wyahardware/hardware/src/main/java/com/wya/hardware/HardWareActivity.java
@@ -0,0 +1,22 @@
+package com.wya.hardware;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wya.wyahardware.R;
+
+/**
+  * 创建日期：2018/11/16 9:14
+  * 作者： Mao Chunjiang
+  * 文件名称：HardWareActivity
+  * 类说明：硬件依赖库
+  */
+ 
+public class HardWareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hard_ware);
+    }
+}
