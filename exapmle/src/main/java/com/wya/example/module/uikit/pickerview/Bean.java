69b60310a6a19aa4114b3de8cd1086a6d44bd1c8
XuDonglin <465715784@qq.com>
2018-12-20 16:15:23 +0800

    pickerView

---
+++ exapmle/src/main/java/com/wya/example/module/uikit/pickerview/Bean.java
@@ -0,0 +1,78 @@
+package com.wya.example.module.uikit.pickerview;
+
+import java.util.List;
+
+/**
+ * author : XuDonglin
+ * e-mail : 465715784@qq.com
+ * time   : 2018/12/20
+ * desc   :
+ * version: 1.0
+ */
+public class Bean {
+
+
+    private List<DataBean> data;
+
+    public List<DataBean> getData() {
+        return data;
+    }
+
+    public void setData(List<DataBean> data) {
+        this.data = data;
+    }
+
+    public static class DataBean {
+        /**
+         * name : 北京市
+         * city : [{"name":"北京市","area":["东城区","西城区","崇文区","宣武区","朝阳区","丰台区","石景山区","海淀区","门头沟区",
+         * "房山区","通州区","顺义区","昌平区","大兴区","平谷区","怀柔区","密云县","延庆县"]}]
+         */
+
+        private String name;
+        private List<CityBean> city;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public List<CityBean> getCity() {
+            return city;
+        }
+
+        public void setCity(List<CityBean> city) {
+            this.city = city;
+        }
+
+        public static class CityBean {
+            /**
+             * name : 北京市
+             * area : ["东城区","西城区","崇文区","宣武区","朝阳区","丰台区","石景山区","海淀区","门头沟区","房山区","通州区","顺义区",
+             * "昌平区","大兴区","平谷区","怀柔区","密云县","延庆县"]
+             */
+
+            private String name;
+            private List<String> area;
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+
+            public List<String> getArea() {
+                return area;
+            }
+
+            public void setArea(List<String> area) {
+                this.area = area;
+            }
+        }
+    }
+}
--- exapmle/src/main/java/com/wya/example/module/uikit/pickerview/PickerViewExampleActivity.java
+++ exapmle/src/main/java/com/wya/example/module/uikit/pickerview/PickerViewExampleActivity.java
@@ -1,96 +1,165 @@
 package com.wya.example.module.uikit.pickerview;
 
-import android.annotation.SuppressLint;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
+import com.google.gson.Gson;
 import com.wya.example.R;
 import com.wya.example.base.BaseActivity;
 import com.wya.uikit.pickerview.CustomPickerView;
 import com.wya.uikit.pickerview.CustomTimePicker;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class PickerViewExampleActivity extends BaseActivity {
 
-	private static final String TAG = "PickerViewExampleActivity";
-	private Button picker_link, time_picker;
-	private List<String> data1 = new ArrayList<>();
-	private List<List<String>> data2 = new ArrayList<>();
-	private List<List<List<String>>> data3 = new ArrayList<>();
+    private static final String TAG = "PickerViewExampleActivity";
+    @BindView(R.id.ymdhms_text)
+    TextView mYmdhmsText;
+    @BindView(R.id.ymd_text)
+    TextView mYmdText;
+    @BindView(R.id.hms_text)
+    TextView mHmsText;
+    @BindView(R.id.hm_space_text)
+    TextView mHmSpaceText;
+    @BindView(R.id.address_text)
+    TextView mAddressText;
+    private List<String> data1 = new ArrayList<>();
+    private List<List<String>> data2 = new ArrayList<>();
+    private List<List<List<String>>> data3 = new ArrayList<>();
+    private CustomTimePicker mCustomTimePicker;
 
-	@Override
-	protected int getLayoutID() {
-		return R.layout.activity_picker_view_example;
-	}
-
-
-	@Override
-	protected void initView() {
-		for (int i = 0; i < 3; i++) {
-			data1.add("data1=" + i);
-			List<String> list = new ArrayList<>();
-			List<List<String>> listList = new ArrayList<>();
-			for (int j = 0; j < 3; j++) {
-				list.add("data1=" + i + "data2=" + j);
-				List<String> list1 = new ArrayList<>();
-				for (int k = 0; k < 3; k++) {
-					list1.add("data1=" + i + "data2=" + j + "data3=" + k);
-				}
-				listList.add(list1);
-			}
-			data2.add(list);
-			data3.add(listList);
-		}
+    @Override
+    protected int getLayoutID() {
+        return R.layout.activity_picker_view_example;
+    }
 
 
-		picker_link = (Button) findViewById(R.id.picker_link);
-		time_picker = (Button) findViewById(R.id.time_picker);
+    @Override
+    protected void initView() {
+
+        ButterKnife.bind(this);
+        setToolBarTitle("PickerView");
+        InputStream inputStream;
+        String data = null;
+        try {
+             inputStream = getAssets().open("data.txt");
+            int size = inputStream.available();
+            byte[] bytes = new byte[size];
+            int len = -1;
+            inputStream.read(bytes);
+            inputStream.close();
+             data = new String(bytes);
+            Log.i("test", ": "+data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
-		picker_link.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				CustomPickerView customPickerView = new CustomPickerView(PickerViewExampleActivity
-						.this, new CustomPickerView.OnChooseItemListener() {
-					@Override
-					public void itemSelected(int position1, int position2, int position3) {
-						Toast.makeText(PickerViewExampleActivity.this,
-								"position1" + position1 + "position2" + position2 + "position3" +
-										"" + position3, Toast.LENGTH_SHORT).show();
-					}
-				});
-				customPickerView.setData(data1, data2, data3);
-				customPickerView.show();
+        Bean bean = new Gson().fromJson(data, Bean.class);
+        for (int i = 0; i < bean.getData().size(); i++) {
+            data1.add(bean.getData().get(i).getName());
+            List<String> list = new ArrayList<>();
+            List<List<String>> listList = new ArrayList<>();
+            for (int j = 0; j < bean.getData().get(i).getCity().size(); j++) {
+                list.add(bean.getData().get(i).getCity().get(j).getName());
+                List<String> list1 = new ArrayList<>();
+                for (int k = 0; k < bean.getData().get(i).getCity().get(j).getArea().size(); k++) {
+                    list1.add(bean.getData().get(i).getCity().get(j).getArea().get(k));
+                }
+                listList.add(list1);
+            }
+            data2.add(list);
+            data3.add(listList);
+        }
 
-			}
-		});
-
-
-		time_picker.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				CustomTimePicker customTimePicker = new CustomTimePicker(PickerViewExampleActivity
-						.this, new CustomTimePicker.OnTimePickerSelectedListener() {
-					@Override
-					public void selected(Date date) {
-						SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-						String format = dateFormat.format(date);
-						Toast.makeText(PickerViewExampleActivity.this, format, Toast.LENGTH_SHORT)
-								.show();
-
-					}
-				});
-				customTimePicker.setType(new boolean[]{true, true, true, false, false, false})
-						.setSecondSpace(5)
-						.show();
-			}
-		});
+    }
 
 
-	}
+    @OnClick({R.id.ymdhms_layout, R.id.ymd_layout, R.id.hms_layout, R.id.hm_space_layout, R.id
+            .address_layout})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.ymdhms_layout:
+                mCustomTimePicker = new CustomTimePicker(PickerViewExampleActivity
+                        .this, new CustomTimePicker.OnTimePickerSelectedListener() {
+                    @Override
+                    public void selected(Date date) {
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                        String format = dateFormat.format(date);
+                        mYmdhmsText.setText(format);
+
+                    }
+                });
+                mCustomTimePicker.setType(new boolean[]{true, true, true, true, true, true}).show();
+                break;
+            case R.id.ymd_layout:
+                mCustomTimePicker = new CustomTimePicker(PickerViewExampleActivity
+                        .this, new CustomTimePicker.OnTimePickerSelectedListener() {
+                    @Override
+                    public void selected(Date date) {
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                        String format = dateFormat.format(date);
+                        mYmdText.setText(format);
+
+                    }
+                });
+                mCustomTimePicker.setType(new boolean[]{true, true, true, false, false, false}).show();
+                break;
+            case R.id.hms_layout:
+                mCustomTimePicker = new CustomTimePicker(PickerViewExampleActivity
+                        .this, new CustomTimePicker.OnTimePickerSelectedListener() {
+                    @Override
+                    public void selected(Date date) {
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+                        String format = dateFormat.format(date);
+                        mHmsText.setText(format);
+
+                    }
+                });
+                mCustomTimePicker.setType(new boolean[]{false, false, false, true, true, true}).show();
+                break;
+            case R.id.hm_space_layout:
+                CustomTimePicker customTimePicker = new CustomTimePicker(PickerViewExampleActivity
+                        .this, new CustomTimePicker.OnTimePickerSelectedListener() {
+                    @Override
+                    public void selected(Date date) {
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
+                        String format = dateFormat.format(date);
+                        mHmSpaceText.setText(format);
+
+                    }
+                });
+                customTimePicker.setType(new boolean[]{false, false, false, true, true, false})
+                        .setMinuteSpace(5)
+                        .show();
+                break;
+            case R.id.address_layout:
+                CustomPickerView customPickerView = new CustomPickerView(PickerViewExampleActivity
+                        .this, new CustomPickerView.OnChooseItemListener() {
+                    @Override
+                    public void itemSelected(int position1, int position2, int position3) {
+                        mAddressText.setText(data1.get(position1)+data2.get(position1).get
+                                (position2)+data3.get(position1).get(position2).get(position3));
+                    }
+                });
+                customPickerView.setData(data1, data2, data3);
+                customPickerView
+                        .setCycle(false)
+                        .show();
+                break;
+        }
+
+
+    }
 }
