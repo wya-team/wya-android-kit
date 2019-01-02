1dbbe7f45d27e6cdc1288b38a126b0e6ae02b684
MaoShine <550612711@qq.com>
2019-01-02 17:04:24 +0800

    Stepper宽度根据max_num动态设置

--- wyauikit/uikit/src/main/java/com/wya/uikit/stepper/WYAStepper.java
+++ wyauikit/uikit/src/main/java/com/wya/uikit/stepper/WYAStepper.java
@@ -7,7 +7,6 @@ import android.support.annotation.Nullable;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
