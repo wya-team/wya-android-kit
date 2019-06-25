537b75b5ab37a0654ba88ac2fcfd3d96300a4618
289811200 <289811200@qq.com>
2019-06-25 11:32:07 +0800

    upload router

--- library/router/router-api/src/main/java/com/weiyian/android/router/utils/Consts.java
+++ library/router/router-api/src/main/java/com/weiyian/android/router/utils/Consts.java
@@ -16,7 +16,7 @@ public final class Consts {
     public static final String SUFFIX_PROVIDERS = "Providers";
     public static final String SUFFIX_AUTOWIRED = SEPARATOR + SDK_NAME + SEPARATOR + "Autowired";
     public static final String DOT = ".";
-    public static final String ROUTE_ROOT_PAKCAGE = "com.alibaba.android.arouter.routes";
+    public static final String ROUTE_ROOT_PAKCAGE = "com.weiyian.android.router.routes";
     
     public static final String AROUTER_SP_CACHE_KEY = "SP_AROUTER_CACHE";
     public static final String AROUTER_SP_KEY_MAP = "ROUTER_MAP";
